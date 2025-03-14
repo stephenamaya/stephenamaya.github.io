420cdab1c5be03c27cd4b8784b4be2273942b292
Stephen Amaya <57957346+stephenamaya@users.noreply.github.com>
2025-03-14 15:23:30 -0400

    Revamp stage 1

---
+++ scripts.js
@@ -0,0 +1,12 @@
+// function getpictures() {}
+// const imgs = document.querySelectorAll(".gallery img");
+// const fullPage = document.querySelector("#fullpage");
+
+// imgs.forEach((img) => {
+//   img.addEventListener("click", function () {
+//     fullPage.style.backgroundImage = "url(" + img.src + ")";
+//     fullPage.style.display = "block";
+//   });
+// });
+
+
